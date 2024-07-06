42286c4f7365f796c0fb5cfc41fa09257dfcc9fd
Mateusz Cyran <mateusz.cyran13@gamil.com>
2024-07-06 15:26:33 +0200

    komendy potrzebne do ukoczenia tutorialu

---
+++ komendy.sh
@@ -0,0 +1,22 @@
+git init
+git add <plik>
+git commit -m "Opis commitu"
+git branch <nazwa_gałęzi>
+git checkout <nazwa_gałęzi>
+git merge <nazwa_gałęzi>
+git branch -d <nazwa_gałęzi>
+git tag <nazwa_tag>
+git log
+git status
+git diff
+git rebase <nazwa_gałęzi>
+git branch
+git config --global user.email "email@dsw.edu.pl"
+git config --global user.name "Twoje Imię"
+git init
+git add commands.sh
+git commit -m "Dodano skrypt z komendami z tutorialu"
+git remote add origin https://github.com/TwójLogin/nazwa-repozytorium.git
+git branch -M main
+git push -u origin main
+git push
